3adc9c10de7385737d1b9122f3387a8669736896
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-15 16:06:34 +0200

    feat(log): add logger

---
+++ internal/log/log.go
@@ -0,0 +1,91 @@
+// Package log provides logging using the zap logger.
+package log
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Options struct {
+	Debug bool
+}
+
+func New(o Options) (*zap.SugaredLogger, error) {
+	core, err := newCore(o)
+	if err != nil {
+		return nil, err
+	}
+
+	return zap.New(core, zapOptions(o)...).Sugar(), nil
+}
+
+func newCore(o Options) (zapcore.Core, error) {
+	lvl := zap.InfoLevel
+	if o.Debug {
+		lvl = zap.DebugLevel
+	}
+
+	enc := newJSONEncoder()
+	if o.Debug {
+		enc = newConsoleEncoder()
+	}
+
+	sink, _, err := zap.Open("stderr")
+	if err != nil {
+		return nil, err
+	}
+
+	core := zapcore.NewCore(enc, sink, zap.NewAtomicLevelAt(lvl))
+	if !o.Debug {
+		core = zapcore.NewSamplerWithOptions(core, time.Second, 100, 100)
+	}
+
+	return core, nil
+}
+
+func zapOptions(o Options) (zopts []zap.Option) {
+	if o.Debug {
+		zopts = append(zopts, zap.Development(), zap.AddStacktrace(zapcore.WarnLevel))
+	} else {
+		zopts = append(zopts, zap.AddStacktrace(zapcore.ErrorLevel))
+	}
+
+	return append(zopts, zap.AddCaller())
+}
+
+func newConsoleEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
+		// console ignores the keys and simply checks if they are non-empty
+		MessageKey:     ".",
+		LevelKey:       ".",
+		TimeKey:        ".",
+		NameKey:        ".",
+		CallerKey:      ".",
+		FunctionKey:    ".",
+		StacktraceKey:  ".",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseColorLevelEncoder,
+		EncodeTime:     zapcore.RFC3339TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	})
+}
+
+func newJSONEncoder() zapcore.Encoder {
+	return zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "lvl",
+		TimeKey:        "time",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		FunctionKey:    zapcore.OmitKey,
+		StacktraceKey:  "stack_trace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.RFC3339TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	})
+}
