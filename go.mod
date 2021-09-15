2feb771562446652140142a5b63877129208d995
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-15 16:06:46 +0200

    feat(config): add config loader

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mavolin/stormy
 
 go 1.16
+
+require (
+	github.com/getsentry/sentry-go v0.11.0 // indirect
+	github.com/knadh/koanf v1.2.2 // indirect
+	github.com/mavolin/adam v0.0.0-20210901004505-94c2f1ba61de // indirect
+	go.uber.org/zap v1.19.0 // indirect
+)
---
+++ internal/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/diamondburned/arikawa/v3/discord"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/json"
+	"github.com/knadh/koanf/providers/env"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
+type (
+	Config struct {
+		BotToken string           `koanf:"bottoken"`
+		Owners   []discord.UserID `koanf:"owners"`
+
+		Status   discord.Status `koanf:"status"`
+		Activity Activity       `koanf:"activity"`
+
+		Sentry Sentry `koanf:"sentry"`
+		Mongo  Mongo  `koanf:"mongo"`
+	}
+
+	Activity struct {
+		Type discord.ActivityType `koanf:"type"`
+		Name string               `koanf:"name"`
+		URL  discord.URL          `koanf:"url"`
+	}
+
+	Sentry struct {
+		DSN        string  `koanf:"dsn"`
+		Server     string  `koanf:"server"`
+		SampleRate float64 `koanf:"samplerate"`
+	}
+
+	Mongo struct {
+		URI    string `koanf:"uri"`
+		DBName string `koanf:"dbname"`
+	}
+)
+
+var defaultConfig = &Config{
+	Status: discord.OnlineStatus,
+}
+
+func Read(configFilePath string) (c *Config, err error) {
+	k := koanf.New(".")
+
+	if err := k.Load(structs.Provider(defaultConfig, "koanf"), nil); err != nil {
+		return nil, err
+	}
+
+	// try to load
+	_ = k.Load(file.Provider(configFilePath), json.Parser())
+
+	err = k.Load(env.Provider("STORMY_", ".", envReplacer("STORMY_")), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, k.Unmarshal("", &c)
+}
+
+func envReplacer(prefix string) func(string) string {
+	return func(s string) string {
+		return strings.ReplaceAll(strings.ToLower(strings.TrimPrefix(prefix, s)), "_", ".")
+	}
+}
