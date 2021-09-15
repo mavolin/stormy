1e8a0cd4e3b24766cb3d25e47846981b0955b54a
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-15 16:06:18 +0200

    feat(meta): add Version

---
+++ internal/meta/meta.go
@@ -0,0 +1,6 @@
+package meta
+
+// Version is the current version.
+// This should be set during compilation using
+// `-ldflags "-X github.com/mavolin/stormy/internal/meta.Version=1.2.3"`.
+var Version = "dev"
