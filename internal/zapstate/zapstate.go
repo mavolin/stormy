6a67088c509f01b670328dcfe73cf5f47c781ab6
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-15 18:53:37 +0200

    feat(zapstate): add zapstate

---
+++ internal/zapstate/zapstate.go
@@ -0,0 +1,29 @@
+package zapstate
+
+import (
+	"github.com/mavolin/disstate/v4/pkg/event"
+	"github.com/mavolin/disstate/v4/pkg/state"
+	"go.uber.org/zap"
+)
+
+type key struct{}
+
+// Get returns the *zap.SugaredLogger previously stored in the base using
+// the middleware returned by Middleware.
+func Get(b *event.Base) *zap.SugaredLogger {
+	if l := b.Get(key{}); l != nil {
+		if l, ok := l.(*zap.SugaredLogger); ok && l != nil {
+			return l
+		}
+	}
+
+	return nil
+}
+
+// Middleware returns a bot.Middleware that adds the passed logger to the
+// event's base.
+func Middleware(l *zap.SugaredLogger) func(*state.State, *event.Base) {
+	return func(_ *state.State, base *event.Base) {
+		base.Set(key{}, l)
+	}
+}
