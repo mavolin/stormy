d3f54eb4e26d9d99e7c7330cf7217c91957ca7bb
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-30 20:52:09 +0200

    feat(action): remove Action.IsEnabled and add .InstanceNames

--- pkg/action/action.go
+++ pkg/action/action.go
@@ -10,9 +10,13 @@ import (
 type Action interface {
 	// Name returns the name of the action.
 	Name() string
-	// IsEnabled reports whether the action is set up in the channel with the
-	// passed id.
-	IsEnabled(channelID discord.ChannelID) bool
+	// InstanceNames returns the names of the instances running in the channel
+	// with the passed id, or nil if the command is disabled.
+	//
+	// As a special case, if an empty slice is returned, the action is
+	// considered as enabled and as only using a single instance per channel by
+	// design.
+	InstanceNames(channelID discord.ChannelID) ([]string, error)
 
 	// Enable enables the action in the invoking channel.
 	//
