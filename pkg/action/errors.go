b065ec415aaf3965c397d0dcdfb1c2f1ec664019
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-09-30 20:07:36 +0200

    feat(action): add DisabledError

--- pkg/action/errors.go
+++ pkg/action/errors.go
@@ -6,6 +6,10 @@ import (
 	"github.com/mavolin/adam/pkg/errors"
 )
 
+// =============================================================================
+// EnabledError
+// =====================================================================================
+
 // EnabledError is the error returned if a user attempts to enable an Action
 // that is already enabled.
 //
@@ -40,4 +44,44 @@ func (e *EnabledError) As(target interface{}) bool {
 
 func (e *EnabledError) AsUserError() *errors.UserError {
 	return errors.NewUserErrorf("`%s` is already enabled in this channel.", e.Name)
+}
+
+// =============================================================================
+// DisabledError
+// =====================================================================================
+
+// DisabledError is the error returned if a user attempts to disable an Action
+// that is already disabled.
+//
+// It makes itself available as a *errors.UserError when calling As.
+type DisabledError struct {
+	// Name is the name of the Action.
+	Name string
+}
+
+var _ error = new(DisabledError)
+
+func NewDisabledError(actionName string) *DisabledError {
+	return &DisabledError{Name: actionName}
+}
+
+func (e *DisabledError) Error() string {
+	return fmt.Sprintf("action: the %s action is already disabled in the invoking channel", e.Name)
+}
+
+func (e *DisabledError) As(target interface{}) bool {
+	switch err := target.(type) {
+	case **errors.UserError:
+		*err = e.AsUserError()
+		return true
+	case *errors.Error:
+		*err = e.AsUserError()
+		return true
+	default:
+		return false
+	}
+}
+
+func (e *DisabledError) AsUserError() *errors.UserError {
+	return errors.NewUserErrorf("`%s` is already disabled in this channel.", e.Name)
 }
--- pkg/action/errors_test.go
+++ pkg/action/errors_test.go
@@ -9,6 +9,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// =============================================================================
+// EnabledError
+// =====================================================================================
+
 func TestEnabledError_As(t *testing.T) {
 	t.Parallel()
 
@@ -31,6 +35,41 @@ func TestEnabledError_As(t *testing.T) {
 			t.Parallel()
 
 			expect := NewEnabledError("abc")
+
+			require.Truef(t, errors.As(expect, c.Target), "errors.As returned false for %T", c.Target)
+
+			asErr := reflect.ValueOf(c.Target).Elem().Interface()
+			assert.Equal(t, expect.AsUserError(), asErr)
+		})
+	}
+}
+
+// =============================================================================
+// DisabledError
+// =====================================================================================
+
+func TestDisabledError_As(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		Name   string
+		Target interface{}
+	}{
+		{
+			Name:   "*errors.Error",
+			Target: new(errors.Error),
+		},
+		{
+			Name:   "**errors.UserError",
+			Target: new(*errors.UserError),
+		},
+	}
+
+	for _, c := range testCases {
+		t.Run(c.Name, func(t *testing.T) {
+			t.Parallel()
+
+			expect := NewDisabledError("abc")
 
 			require.Truef(t, errors.As(expect, c.Target), "errors.As returned false for %T", c.Target)
 
