26e2d34d2feeca99a55e88f4cbb9e5550a143eb9
Maximilian von Lindern <maximilian.v.lindern@gmail.com>
2021-10-17 13:27:53 +0200

    feat(stdcolor): add more default colors

--- internal/common/common.go
+++
@@ -1,5 +0,0 @@
-package common
-
-import "github.com/diamondburned/arikawa/v3/discord"
-
-var EnabledColor discord.Color = 0x46cc5c
---
+++ internal/stdcolor/stdcolor.go
@@ -0,0 +1,10 @@
+package stdcolor
+
+import "github.com/diamondburned/arikawa/v3/discord"
+
+var (
+	Default discord.Color = 0x44c5f3
+	Green   discord.Color = 0x46cc5c
+	Red     discord.Color = 0xff6344
+	Yellow  discord.Color = 0xf99639
+)
